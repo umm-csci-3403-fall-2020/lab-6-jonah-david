9ba3713d3cc836deb6fd9c5d7abe001b60795e23
Jonah-Northwood <north305@morris.umn.edu>
2020-10-15 11:33:56 -0500

    Create server_test.sh for testing, set up executors
    Currently getting an error with IOExeceptions not working

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,27 +5,50 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
 	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013; 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
 	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+			ExecutorService executor = Executors.newFixedThreadPool(10);
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
+			while (true) {
+				Socket socket = serverSocket.accept();
+
+				System.out.println("Got a request!");
+
+				executor.execute(new Runnable() {
+					public void run() {
+						//Input from the client
+						InputStream input = socket.getInputStream();
+
+						// Output to send to the client
+						OutputStream output = socket.getOutputStream();
+						int inputByte;
+					
+						while ((inputByte = input.read()) != -1) {
+						  output.flush();
+						  output.write(inputByte);
+						}
+					}
+				});
+			
+				// Close the client socket since we're done.
+				executor.shutdown();
+				socket.close();
+		  	}
+		} catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+      		System.err.println(ioe);
 		}
 	}
 }
