5029eb057b70dd64eac6bf3ea177fb286482a5e4
david <lucht013@morris.umn.edu>
2020-11-26 15:13:29 -0600

    Implement functional echoclient and echoserver

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,5 +1,7 @@
 package echoserver;
 
+import org.omg.CosNaming.BindingListHelper;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,23 +11,60 @@ import java.net.Socket;
 public class EchoServer {
 	
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013;
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
 	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+		ServerSocket sock = new ServerSocket(PORT_NUMBER);
+		while(true){
+			//wait until someone accepts
+			Socket client = sock.accept();
+			System.out.println("HEY! We got somebody!");
+			//Input from the client
+			InputStream in = client.getInputStream();
+			// Output to send to the client
+			OutputStream out = client.getOutputStream();
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
+			ServerInstance instance = new ServerInstance(client, in, out);
+			Thread newClient = new Thread(instance);
+
+			newClient.start();
+		}
+	}
+
+	public class ServerInstance implements Runnable {
+		Socket client;
+		InputStream input;
+		OutputStream output;
+
+
+		public ServerInstance(Socket client,InputStream in,OutputStream out){
+			this.client = client;
+			input = in;
+			output = out;
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					int inputByte;
+
+					while ((inputByte = input.read()) != -1) {
+						//System.out.println("Got a byte: " + inputByte);
+						output.flush();
+						output.write(inputByte);
+					}
+
+					// Close the client socket since we're done.
+					client.close();
+				}
+			} catch (IOException e) {
+				System.out.println("Take a look at ServerInstance, something went wrong.");
+			}
 		}
 	}
 }
